dcc640fb8c25e8d1ef86d96c855dd76b17228551
Lucas Souza <lucas.souza@devplace.com.br>
2019-07-12 09:20:05 -0300

    Inicio da codificação de algoritmos genéticos - maximização de função;

---
+++ AlgoritimoGenetico/Class/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritimoGenetico.Class
+{
+    public abstract class Constants
+    {
+        static public int sizeChromosome = 9;
+        static public int sizePopulation = 500;
+        static public int functionXSize = (int)Math.Pow(2, sizeChromosome);
+        public static Random random = new Random((int)DateTime.Now.Ticks);
+
+        public static double Function1(double x)
+        {
+            return (double)(100 + Math.Abs(x * Math.Sin(Math.Sqrt(Math.Abs(x)))));
+        }
+    }
+}
