1475d93fbdd7c826c737191db78c99dc2fc44867
Lucas Souza <lucas.souza@devplace.com.br>
2019-08-02 09:08:43 -0300

    Iniciado projeto do caixeiro viajante.

---
+++ TSP - Caixeiro Viajante/TSP/TSP/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPopulationSize = new System.Windows.Forms.MaskedTextBox();
+            this.txtCrossOverTax = new System.Windows.Forms.MaskedTextBox();
+            this.txtMutationTax = new System.Windows.Forms.MaskedTextBox();
+            this.txtEvolution = new System.Windows.Forms.MaskedTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.txtEvolution);
+            this.panel1.Controls.Add(this.txtMutationTax);
+            this.panel1.Controls.Add(this.txtCrossOverTax);
+            this.panel1.Controls.Add(this.txtPopulationSize);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(439, 530);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tam. População:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tax. Cruzamento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tax. Mutação:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Evoluções:";
+            // 
+            // txtPopulationSize
+            // 
+            this.txtPopulationSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPopulationSize.Location = new System.Drawing.Point(172, 9);
+            this.txtPopulationSize.Name = "txtPopulationSize";
+            this.txtPopulationSize.Size = new System.Drawing.Size(177, 26);
+            this.txtPopulationSize.TabIndex = 4;
+            // 
+            // txtCrossOverTax
+            // 
+            this.txtCrossOverTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCrossOverTax.Location = new System.Drawing.Point(172, 39);
+            this.txtCrossOverTax.Name = "txtCrossOverTax";
+            this.txtCrossOverTax.Size = new System.Drawing.Size(177, 26);
+            this.txtCrossOverTax.TabIndex = 5;
+            // 
+            // txtMutationTax
+            // 
+            this.txtMutationTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMutationTax.Location = new System.Drawing.Point(172, 70);
+            this.txtMutationTax.Name = "txtMutationTax";
+            this.txtMutationTax.Size = new System.Drawing.Size(177, 26);
+            this.txtMutationTax.TabIndex = 6;
+            // 
+            // txtEvolution
+            // 
+            this.txtEvolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEvolution.Location = new System.Drawing.Point(172, 102);
+            this.txtEvolution.Name = "txtEvolution";
+            this.txtEvolution.Size = new System.Drawing.Size(177, 26);
+            this.txtEvolution.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1033, 523);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox txtEvolution;
+        private System.Windows.Forms.MaskedTextBox txtMutationTax;
+        private System.Windows.Forms.MaskedTextBox txtCrossOverTax;
+        private System.Windows.Forms.MaskedTextBox txtPopulationSize;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
